91cfe7e58ecba299bebbde84b1f85532a71a7672
daserrano <sein2809@gmail.com>
2023-09-23 14:02:24 +0200

    [FEAT] Añadido unicafe paso 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,10 +9,39 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
+  const handleGood = () => {
+    const updateGood = good + 1;
+    setGood(updateGood)
+
+    const total = all + 1;
+    setAll(total)
+    setAverage((updateGood-bad)/total)
+
+    setPositive((updateGood/total)*100)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+
+    const total = all + 1;
+    setAll(total)
+    setAverage((good-bad)/total)
+
+    setPositive((good/total)*100)
+  }
+  const handleBad = () => {
+    const updateBad = bad + 1;
+    setBad(updateBad)
+
+    const total = all + 1;
+    setAll(total)
+    setAverage((good-updateBad)/total)
+
+    setPositive((good/total)*100)
+  }
 
   return (
     <main>
@@ -25,6 +54,10 @@ function App() {
       <h3>good {good}</h3>
       <h3>neutral {neutral}</h3>
       <h3>bad {bad}</h3>
+
+      <h3>all {all}</h3>
+      <h3>average {average}</h3>
+      <h3>positive {positive} %</h3>
     </main>
   )
 }
