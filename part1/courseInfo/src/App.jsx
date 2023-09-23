b48ea8d58c3a7bafde171fa3921cdce81e0e6c95
daserrano <sein2809@gmail.com>
2023-09-23 16:35:09 +0200

    [FEAT] Añadido courseInfo

---
+++ part1/courseInfo/src/App.jsx
@@ -0,0 +1,53 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Content = ({ parts }) => {
+    return (
+        <main>
+            {parts.map((part, index) => (
+                <p key={index}>
+                    {part.name} {part.exercises}
+                </p>
+            ))}
+        </main>
+    )
+}
+
+const Total = ({ parts }) => {
+    const total = parts.reduce((accumulator, part) => accumulator + part.exercises, 0)
+    return (
+        <p>Number of exercises {total}</p>
+    )
+}
+
+
+
+const App = () => {
+
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+export default App
