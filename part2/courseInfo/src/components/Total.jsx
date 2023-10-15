a8d8af360b82e237ef010f81495bb79947f3d60e
daserrano <sein2809@gmail.com>
2023-10-15 10:22:13 +0200

    [REF] Modificado reduce para mostrar totales

--- part2/courseInfo/src/components/Total.jsx
+++ part2/courseInfo/src/components/Total.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
 const Total = ({ parts }) => {
-    // const total = parts.map(part => part.exercises.reduce((sum, acc) => sum + acc, 0)); 
-    const totals = parts.map(part => part.exercises);
-    const numbers = totals.reduce((sum, acc) => sum + acc, 0)
+    const numbers = parts.reduce((sum , acc) => sum + acc.exercises, 0)
+    
     return (
         <b>Total of {numbers} exercises</b>
     )
