68c05db610b4d177f875c5b0df5f21c8a78bfcc2
daserrano <sein2809@gmail.com>
2023-10-15 13:06:31 +0200

    [FEAT] Añadido ejercicio 2.7

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -9,6 +9,8 @@ function App() {
 
   const addName = (event) => {
     event.preventDefault();
+
+    if(persons.find(person => person.name === newName)) return alert(`${newName} is already added to phonebook`)
     
     const addName = {
       name: newName,
