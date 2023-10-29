9e47282a7a37245e973835b61fb2229e0aea7a2b
daserrano <sein2809@gmail.com>
2023-10-29 09:41:50 +0100

    [FEAT] Añadido axios para peticiones a servidor. 2.11

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', key: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', key: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', key: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', key: 4 }
-  ])
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilterValue] = useState('')
 
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => setPersons(response.data))
+  }, [])
   const addPerson = (event) => {
     event.preventDefault();
 
