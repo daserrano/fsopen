68f4509e8ea2f195e07379e498cf1cfbaa1830b1
daserrano <sein2809@gmail.com>
2023-12-02 12:37:05 +0100

    [FEAT] Añadido ejercicio 2.19

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -5,12 +5,14 @@ import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import axios from 'axios'
 import personsService from '../src/services/persons'
+import Alert from './components/Alert'
 
 function App() {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilterValue] = useState('')
+  const [message, setMessage] = useState('')
 
   useEffect(() => {
     personsService.getAll().then(response => setPersons(response))
@@ -32,12 +34,22 @@ function App() {
       id: personExists ? personExists.id : null
     }
     confirm ?
-    personsService.update(addPerson).then(updatePerson => {
-      setPersons(persons.filter(person => person.id !== updatePerson.id).concat(updatePerson))
-    })
+    personsService.update(addPerson)
+      .then(updatePerson => {
+        setPersons(persons.filter(person => person.id !== updatePerson.id).concat(updatePerson))
+        createMessage(`Modified ${updatePerson.name}`)
+      })
     :
-    personsService.create(addPerson).then(newPerson => setPersons(persons.concat(newPerson)))
+    personsService.create(addPerson).then(newPerson => {
+      setPersons(persons.concat(newPerson))
+      createMessage(`Added ${addPerson.name}`)
+    })
     clearInputs()
+  }
+
+  const createMessage = (text) => {
+    setMessage(text)
+    setTimeout(()=>{setMessage(null)},5000)
   }
 
   const deletePerson = (id) => {
@@ -60,7 +72,8 @@ function App() {
 
   return (
     <div style={{border: "1px solid #e7e7e7", borderRadius: "20px", padding: "20px", backgroundColor: "#f9f9e9"}}>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <Alert message={message} />
       <Filter filterValue={filterValue} handleInput={handleInput} />
 
       <PersonForm
---
+++ part2/phoneBook/src/components/Alert.jsx
@@ -0,0 +1,11 @@
+import '../index.css'
+
+const Alert = ({ message }) => {
+  return (
+    message && <div className="alert">
+        {message}
+    </div>
+  )
+}
+
+export default Alert
